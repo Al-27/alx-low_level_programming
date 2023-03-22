18f7f8a3b61cca78850bfcebe27058f41221d546
Al-27 <ahmedlaghfour@gmail.com>
2023-03-22 16:20:01 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
- * my_function - This is a description
+ * _islower - Makes the sum of two numbers
+ * @c: First operand
+ * @arg2: Second operand
+ *
+ * Return: The sum of the two parameters
  */
 int _islower(int c){
 	return c <= 'z' && c >= 'a' ? 1 : 0;
