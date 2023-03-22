de7cbc41768cfe6fa0d3b2b20572f1f0bcc7c892
Al-27 <ahmedlaghfour@gmail.com>
2023-03-22 16:23:47 -0700

    task4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - Makes the sum of two numbers
+ * @c: First operand
+ *
+ * Return: 1
+ */
+int _isalpha(int c){
+	return (c <= 'z' && c >= 'a') || (c <= 'Z' && c >= 'A' ) ? 1 : 0;
+}
