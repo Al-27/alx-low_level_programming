2f46e95d2bc4823c478c31bc2d63328048944910
Al-27 <ahmedlaghfour@gmail.com>
2023-03-22 20:22:48 -0700

    task 100 Fix2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,23 +12,26 @@ void print_times_table(int n){
 	
 	for(i=0; i < MAX; i++){
 		
-		_putchar('0'); 
-		_putchar(','); 
-		for(j = 1; j < MAX; j++){
+		putchar('0'); 
+		putchar(','); 
+		for(j = 0; j < MAX; j++){
 			
 			sum = i * j;
 			
 			ones = (sum % 10);
 			tens = sum / 10  == 0 ?' ' : '0'+(sum)/10 ;
 			hund = sum / 100  == 0 ?' ' : '0'+(sum)/100 ;
-			_putchar(' '); 
-			_putchar(hund); 
-			_putchar(tens); 
-			_putchar(ones +'0'); 
+			if(hund >= '1'){
+				tens = (sum % 100 - ones)/10 + '0'; 
+			}
+			putchar(' '); 
+			putchar(hund); 
+			putchar(tens); 
+			putchar(ones +'0'); 
 			if(j == n)
-				_putchar('\n');
+				putchar('\n');
 			else{
-				_putchar(','); 
+				putchar(','); 
 			} 
 			
 		}
