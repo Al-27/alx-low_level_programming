d449e203eee25d863caa3cb4abd90cb432fe7336
Al-27 <ahmedlaghfour@gmail.com>
2023-03-23 08:00:32 -0700

    fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,33 +1,35 @@
 #include "main.h"
 /**
  * print_to_98 - This is a rand desci
- * @a: First parameter
+ * @n: First parameter
  *
  */
 void print_to_98(int n){
-	 
-	
-	while(1){
-		
-		if(n >= 98 ){
-			printf("%d",n--);
-			if(n < 98){
-				putchar('\n');
-				break;
-			}
-			printf(", ");
-		}
-		else{
-		
-			printf("%d", n++);
-			
-			if(n>98){
-				putchar('\n');
-				break;
-			}		
-			printf(", ");
-		}
-	}
-		
-	
+         
+
+        while(1){
+
+                if(n >= 98 ){
+                        _putchar((n--) + '0');
+                        if(n < 98){
+                                _putchar('\n');
+                                break;
+                        }
+                        _putchar(",");
+                        _putchar(" ");
+                }
+                else{
+
+                        _putchar((n++) + '0');
+
+                        if(n>98){
+                                _putchar('\n');
+                                break;
+                        }
+                        _putchar(",");
+                        _putchar(" ");
+                }
+        }
+
+
 }
