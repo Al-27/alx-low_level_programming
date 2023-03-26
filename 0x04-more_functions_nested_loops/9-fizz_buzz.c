f40e73e1bcc5c8e6fd9ac4f82db2f0101cf27206
Al-27 <ahmedlaghfour@gmail.com>
2023-03-26 15:03:25 -0700

    tsk 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+/* more headers goes there */
+
+/* betty style doc for function main goes there */ 
+ 
+int main(void)
+{
+	
+	int i; 
+	i = 1;
+	while (i <= 100){
+		if(i%3 == i%5 && i%3 == 0){
+			printf("FizzBuzz ");
+		}
+		else if(i%3==0)
+			printf("Fizz ");
+		else if(i%5==0)
+			printf("Buzz ");
+		else
+			printf("%d ",i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
