fcb157fdc74a40e2fb974c94331273f7114e3a52
Al-27 <ahmedlaghfour@gmail.com>
2023-03-26 14:36:02 -0700

    tsk 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - This is a rand desci
+ * @size: int
+ *
+ */
+void print_square(int size){
+	int i,j;
+	j = i = n;
+	if(n <= 0)
+		_putchar(10);
+	while(n>0){
+		i = j;
+		while(i>0){
+			_putchar('#');
+			i--;
+		} 
+		n--;
+	_putchar(10);
+		
+	}
+	
+	
+	
+}
