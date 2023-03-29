20f9b71586f16dfb78c34f5bf44141ccae690a9f
Al-27 <ahmedlaghfour@gmail.com>
2023-03-28 20:31:05 -0700

    0x05

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h" 
+/**
+ * _strlen - This is a rand desci
+ * @s: arg1
+ *
+ * Return: i
+ */
+int _strlen(char *s)
+{
+ 	int i;
+ 	i = 0;
+ 	while(s[i]!='\0'){
+ 		i++;
+ 	}
+ 	return i;
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h" 
+/**
+ * _puts - This is a rand desci
+ * @str: arg1
+ *
+ */
+void _puts(char *str)
+{
+ 	int i;
+ 	i = 0;
+ 	while(s[i]!='\0'){
+ 		_putchar(s[i]);
+ 	}
+ 	_putchar(10);
+}
