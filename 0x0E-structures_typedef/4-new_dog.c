c6c18b42f3121d656e894ab03fa9987072a5942c
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-04-18 13:05:35 +0000

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,17 +10,22 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *tmp = malloc(sizeof(dog_t));
+	char *cp_owner, *cp_name;
+	
 	if(tmp)
 	{
 		tmp->name = malloc(250);
 		tmp->owner = malloc(250);
-		if(tmp->name && tmp->owner)
+		cp_name = malloc(250);
+		cp_owner = malloc(250);
+		if(tmp->name && tmp->owner && cp_name && cp_owner)
 		{
-			tmp->name = name;
-			tmp->owner = owner;
+			cp_name = tmp->name = name;
+			cp_owner = tmp->owner = owner;
 			tmp->age = age;	
 		}	
 	}
-	
+	else
+		free(tmp);
 	return tmp;
 }
