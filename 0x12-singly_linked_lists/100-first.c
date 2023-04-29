5ea13c68759000f5164419242625c8d6c960349e
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-04-29 01:23:27 +0000

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,13 +9,4 @@ void __attribute__((constructor)) pmain()
 {
     printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
-}
+
