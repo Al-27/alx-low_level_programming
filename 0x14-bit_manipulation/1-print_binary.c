c52ea07a1878c53ac33b1a56873eb97db4b07a87
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-07-08 01:26:18 +0000

    print number as bin to console

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h" 
+ 
+/** 
+* print_binary - desc  
+* @arg: arg  
+*  
+* Return: type  
+*/ 
+void print_binary(unsigned long int n)
+{
+    
+    int i = sizeof(ulInt) * 8 - 1;
+    int init = 0;
+    char c0;
+    
+    for(;i >= 0 ; i--)
+    {
+        if( (!init) && (n >> i & 0) )
+        	continue;
+        else if( (!init) && (n >> i & 1) )
+        {
+            init = 1;
+        }
+        
+        if( init ){
+            c0 = 48 + (n >> i & 1);
+            write(1,&c0,1);
+   		}
+    }
+
+}
