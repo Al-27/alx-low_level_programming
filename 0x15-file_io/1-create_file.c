b7ad64972a7e15ec8a18b28f559877274db83b34
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-07-14 03:39:26 +0100

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,6 @@
 int create_file(const char *filename, char *text_content)
 {
     FILE *file = NULL;
-    FILE *tmp = malloc(sizeof(FILE));
     struct stat sfile;
     int len = 0, written = 0;
     if( !filename )
