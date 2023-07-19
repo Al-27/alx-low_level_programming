b995d3fbcded55f50fc2ac7eb2a769f2b3a9895c
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-07-19 06:44:21 +0100

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
+/**
+ * get_endianness - func
+ * 
+ * Return: int
+ */
 int get_endianness()
 {
     
-    if( __BYTE_ORDER == __LITTLE_ENDIAN )
+    if( __BYTE_ORDER == 1234 )
         return 1;
     
     return 0;
