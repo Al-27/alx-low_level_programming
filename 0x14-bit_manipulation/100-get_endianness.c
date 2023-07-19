cf01963a3c84faaf66d5d8d04d13dba49c00577c
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-07-19 06:52:32 +0100

    Update 100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,15 +1,15 @@
 #include "main.h"
-
 /**
  * get_endianness - func
- * 
+ *
  * Return: int
  */
 int get_endianness()
 {
     char a = 1;
-    if( 00000001 == a )
+
+    if (00000001 == a)
         return 1;
-    
+
     return 0;
 }
