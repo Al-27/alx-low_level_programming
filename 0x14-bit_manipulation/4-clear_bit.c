505edc2cd397400bcab98a0c4fa9057e6d7a7a35
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-07-19 08:00:30 +0100

    Update 4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,21 +1,26 @@
 #include "main.h" 
  
 /** 
-* - desc  
-* @arg: arg  
+* clear_bit - desc  
+* @n: arg  
+* @index: arg  
 *  
 * Return: type  
 */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	ulInt num,  clear = ~1;
+	index = index == 0 ? return 1 : index-1;
 	
 	if(index > sizeof(*n)*8-1 )
 		return -1;
-	
+	/*
 	num = *n >> index; 
 	num = (num & clear) << index;
-	
+	*/
+	clear = 0;
+	clear = (((clear >> index)) + 1) << index; 
+	num = clear ^ *n;
 	
 	*n = num ;
 	return 1;
