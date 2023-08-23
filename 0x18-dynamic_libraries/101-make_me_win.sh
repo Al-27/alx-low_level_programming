866d580207c660521d512d266b1e8f9475831114
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-08-23 01:17:11 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://raw.github.com/Al-27/alx-low_level_programming/master/0x18-dynamic_libraries/libjackpot.so
-export LD_PRELOAD="$PWD/libinjection.so"
+wget -P .. https://raw.github.com/Al-27/alx-low_level_programming/master/0x18-dynamic_libraries/libjackpot.so
+export LD_PRELOAD="$PWD/../libinjection.so"
