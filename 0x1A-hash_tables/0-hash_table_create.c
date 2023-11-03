d13d9fbcead43808c7f6ad8eeb9c460563d9b69c
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-11-03 19:51:08 +0000

    Add files via upload

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *table = NULL;
+    
+    if(size > 0)
+    {
+        table = malloc( sizeof(hash_table_t) );
+        
+        if( table )
+        {
+            table->size = size;
+            table->array = malloc(sizeof(hash_node_t*) * size);
+        } 
+    }
+    return table;
+}
