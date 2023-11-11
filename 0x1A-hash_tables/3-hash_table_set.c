20716090815dcc143c1b0eac9fa8707d0c1f9e41
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-11-11 01:35:13 +0000

    Set function Corrected

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,8 +11,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
         if(valid_key(key))
         {
             index = key_index((u_char*) key,ht->size);
-            isEnd = index == ht->size-1 ? 1 : 0;
             tmp = ht->array[index];
+            
             if(tmp)
             {
                 if(!strcmp( key, tmp->key ))
@@ -23,32 +23,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
                 }
                 else
                 {
-                    index = 0;
                     
-                    while(1)
+                    node = malloc(sizeof(hash_node_t));
+                    
+                    if(node)
                     {
-                        node = ht->array[index];
+                        node->key = strdup(key);
+                        node->value = strdup(value);
+                        node->next = tmp;
+                        ht->array[index] = node;
                         
-                        if(!node && index < ht->size)
-                        {
-                            node = malloc(sizeof(hash_node_t));
-                
-                            if(node)
-                            {
-                                node->key = strdup(key);
-                                node->value = strdup(value);
-                                node->next = isEnd ? NULL : ht->array[index+1];
-                                ht->array[index] = node;
-                                success =  1; 
-                                break;
-                            } 
-                            
-                        }
-                        else if(node)
-                            index++;
-                        else
-                            break;                      
-                    }                    
+                        success =  1;
+                    }
+                    else
+                        success = 0;                
                 }
             }
             else
