fe413c29c72f4651d44ce5fc16aae99cc64cc28c
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-11-11 02:05:26 +0000

    Update 2-key_index.c

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * key_index - func
+ * @key: arg
+ * @size: arg
+ *
+ * Return: int
+*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
     u_long iKey = 0;
