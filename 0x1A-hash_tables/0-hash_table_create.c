cdb29539843940f884083488b2fd639fbf5db6ca
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-11-11 02:01:59 +0000

    adding docs
    still can't comprehend why we don't use doxygen

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - func
+ * @size: arg
+ *
+ * Return: hash_table_t
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
     hash_table_t *table = NULL;
