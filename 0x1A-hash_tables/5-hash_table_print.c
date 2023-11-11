2b973bbe6da1d6cdef50008881964c834ea6faba
Al-27 <125827852+Al-27@users.noreply.github.com>
2023-11-11 01:38:45 +0000

    Print Function fixed

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -4,23 +4,27 @@ void hash_table_print(const hash_table_t *ht)
 {
     u_long i = 0, printed = 0;
     hash_node_t *tmp = NULL;
-    printf("{");
     
     if(ht)
     {
+        printf("{");
+        
         for(;i < ht->size;i++)
         {
             tmp = ht->array[i];
             
-            if(tmp)
+            while(tmp)
             {
                 if(printed) printf(", ");
                 
                 printf("'%s': '%s'",tmp->key,tmp->value);
                 printed=1;
+                
+                tmp = tmp->next;
             }
         }
+        
+        printf("}\n");
     }
     
-    printf("}\n");
 }
