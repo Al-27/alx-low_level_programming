f9eb9ea9128be1304444fb6e08741503aa7c3a4c
Al_27 <ahmedlaghfour@gmail.com>
2024-02-06 20:32:00 +0000

    0x1E

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - func
+ * @array: int*
+ * @size: size_t
+ * @value: int
+ * 
+ * Return: int
+*/
+int linear_search(int *array, size_t size, int value)
+{
+    int i = 0, val = 0;
+    
+    if(array)
+    {
+        for(; i < size; i++)
+        {
+            val = array[i];
+            printf("Value checked array[%d] = [%d]\n",i,val);
+            
+            if(val == value)
+                return i;
+        }
+    }
+    
+    return -1;
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,47 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - func
+ * @array: int*
+ * @size: size_t
+ * @value: int
+ * 
+ * Return: int
+*/
+int binary_search(int *array, size_t size, int value)
+{
+    int beg = 0, cur = 0, end = (int)size, val;
+    while (array && size > 0)
+    {
+        printf("Searching in array: ");
+        print_array(array, beg, end);
+        cur = ( beg + end ) / 2;
+        val = array[cur];
+        
+        if(val == value)
+            return cur;
+        else if( value > val)
+        {
+            beg = cur + 1;
+        }
+        else
+        {
+            end = cur - 1;
+        }
+    }
+    
+    return -1;
+}
+
+/**
+ * print_array - func
+ * @array: int*
+ * @beg: int
+ * @end: int
+*/
+void print_array(int *array, int beg, int end)
+{
+    int i = beg;
+    for(;i < end; i++)
+        printf("%d%s", array[i], i+1 == end ? "\n" : ", " );
+}
