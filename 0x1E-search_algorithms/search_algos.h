9a764b92f0a4861a9dd96c4231bc54bbaca2cf4a
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-02-11 01:02:11 +0000

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,8 +7,10 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 void print_array(int *array, int beg, int end);
 int has_occurences(int *array, int size, int val, int *m);
+
 
 #endif 
