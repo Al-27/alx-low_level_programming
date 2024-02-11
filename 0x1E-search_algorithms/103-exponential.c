ec6cb3d82a5f6ed81312e062af10be8300dbc4c6
Al-27 <125827852+Al-27@users.noreply.github.com>
2024-02-11 02:39:31 +0000

    Create 103-exponential.c

---
+++ 0x1E-search_algorithms/103-exponential.c
@@ -0,0 +1,76 @@
+#include "search_algos.h"
+#include <math.h>
+
+/**
+ * print_array - func
+ * @array: int*
+ * @beg: int
+ * @end: int
+*/
+void print_array(int *array, int beg, int end)
+{
+    int i = beg;
+    for(;i < end; i++)
+        printf("%d%s", array[i], i+1 == end ? "\n" : ", " );
+}
+/**
+ * binary_search - func
+ * @array: int*
+ * @size: size_t
+ * @value: int
+ * 
+ * Return: int
+*/
+int binary_search(int *array, size_t size, int value)
+{
+    int beg = 0, cur = 0, end = (int)size - 1, val, final = 0;
+    while (array && !final)
+    {
+        if( beg == end )
+            if(final == 0 )
+                final = 1;
+        
+        printf("Searching in array: ");
+        print_array(array, beg, end+1);
+        cur = ( beg + end ) / 2;
+        val = array[cur];
+        
+        if(val == value)
+            return cur;
+        else if( value > val)
+        {
+            beg = cur + 1;
+        }
+        else
+        {
+            end = cur - 1;
+        }
+    }
+    
+    return -1;
+}
+/**
+ * exponential_search - func
+ * @array: int*
+ * @size: size_t
+ * @value: int
+ * 
+ * Return: int
+*/
+int exponential_search(int *array, size_t size, int value)
+{
+    int left = 0, cur = 1, sz = (int)size, i;
+	if(array)
+	{
+		while(array[cur] < value && cur < sz)
+		{
+			printf("Value checked array[%d] = [%d]\n",cur,array[cur] ); 
+			left = cur;
+			cur *= 2;			
+		}
+		printf("Value found between indexes [%d] and [%d]\n", left,(int)fmin(cur,sz-1));
+	}
+	sz -= left;
+	i = binary_search(array+left,(size_t)sz,value);
+   return i >= 0 ? i + left: i ;
+}
